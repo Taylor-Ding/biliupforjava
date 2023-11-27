c7b8155f94481ecfeedcb5550ac896d975343f0b
mwxmmy <admin@sshh.top>
2023-11-27 15:02:55 +0800

    修复循环依赖问题

--- src/main/java/top/sshh/bililiverecoder/service/impl/AppRecordPartBilibiliUploadService.java
+++ src/main/java/top/sshh/bililiverecoder/service/impl/AppRecordPartBilibiliUploadService.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import top.sshh.bililiverecoder.entity.BiliBiliUser;
 import top.sshh.bililiverecoder.entity.RecordHistory;
@@ -78,6 +79,7 @@ public class AppRecordPartBilibiliUploadService implements RecordPartUploadServi
     private RecordHistoryRepository historyRepository;
     @Autowired
     private RecordRoomRepository roomRepository;
+    @Lazy
     @Autowired
     private UploadServiceFactory uploadServiceFactory;
 
--- src/main/java/top/sshh/bililiverecoder/service/impl/KodoRecordPartBilibiliUploadService.java
+++ src/main/java/top/sshh/bililiverecoder/service/impl/KodoRecordPartBilibiliUploadService.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import top.sshh.bililiverecoder.entity.BiliBiliUser;
 import top.sshh.bililiverecoder.entity.RecordHistory;
@@ -84,6 +85,7 @@ public class KodoRecordPartBilibiliUploadService implements RecordPartUploadServ
     @Autowired
     private RecordRoomRepository roomRepository;
 
+    @Lazy
     @Autowired
     private UploadServiceFactory uploadServiceFactory;
 
